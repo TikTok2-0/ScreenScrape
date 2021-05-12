e92b8a5cbcf4bbe1ffb64b7b25d36635312c0af6
y1music <yannikschmidt@icloud.com>
2021-05-12 21:40:26 +0200

    f

--- gitPush.py
+++ gitPush.py
@@ -7,15 +7,13 @@ username = "y1music"
 password = "6ce1d3eef6e7ab177a641dda5966f5ef467c4236"
 remote = f"https://{username}:{password}@github.com/TikTok2-0/ScreenScrape.git"
 
-Repo.clone_from(remote, repoPath)
-
 def git_push():
     try:
         repo = Repo(repoPath)
         repo.git.add(update=True)   
         repo.index.commit(messageCommit)
         origin = repo.remote(name='origin')
-        origin.push()
+        origin.push(remote, repoPath)
     except:
         print('Some error occured while pushing the code')    
 
