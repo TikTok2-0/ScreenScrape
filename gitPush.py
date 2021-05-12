d87c3700525f632b3ee2441be3f388aa126824b5
kfuadmin <kfuadmin@VM-Ubuntu>
2021-05-12 21:42:39 +0200

    Update News

--- gitPush.py
+++ gitPush.py
@@ -8,13 +8,11 @@ password = "6ce1d3eef6e7ab177a641dda5966f5ef467c4236"
 remote = f"https://{username}:{password}@github.com/TikTok2-0/ScreenScrape.git"
 
 def git_push():
-    try:
-        repo = Repo(repoPath)
-        repo.git.add(update=True)   
-        repo.index.commit(messageCommit)
-        origin = repo.remote(name='origin')
-        origin.push(remote, repoPath)
-    except:
-        print('Some error occured while pushing the code')    
+
+	repo = Repo(repoPath)
+	repo.git.add(update=True)   
+	repo.index.commit(messageCommit)
+	origin = repo.remote(name='origin')
+	origin.push(remote, repoPath) 
 
 git_push()
