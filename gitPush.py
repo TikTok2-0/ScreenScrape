1b6ebae2851dca111eeea08c8e1ca4964d757129
y1music <y1.music.spam@gmail.com>
2021-05-13 17:28:54 +0200

    Update News

--- gitPush.py
+++ gitPush.py
@@ -1,7 +1,8 @@
 import os
 
-os.chdir('Downloads')
+os.chdir("Downloads")
 os.chdir('ScreenScrape')
+os.system('git add .')
+os.system('git commit -m "Update News"')
 os.system("git push https://y1music:G4Ka38gn7E3TNKv2rksJqFyt@github.com/TikTok2-0/ScreenScrape.git --all")
 
-#Test3
