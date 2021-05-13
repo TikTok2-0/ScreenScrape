7826ec14aecbb23d25e1733d9d8bc096857772d8
y1music <y1.music.spam@gmail.com>
2021-05-13 17:13:47 +0200

    asf

--- gitPush.py
+++ gitPush.py
@@ -1,23 +1,5 @@
-from git import Repo
 import os
 
-repoPath = r'/home/kfuadmin/Downloads/ScreenScrape'
-messageCommit = 'Update News'
-
-username = "y1music"
-password = "6ce1d3eef6e7ab177a641dda5966f5ef467c4236"
-remote = f"https://{username}:{password}@github.com/TikTok2-0/ScreenScrape.git"
-
-def git_push():
-
-	repo = Repo(repoPath)
-	repo.git.add(update=True)   
-	repo.index.commit(messageCommit)
-	origin = repo.remote(name='origin')
-	origin.push() 
-	os.system("y1music\n")
-	os.system("16802003Ys!\n")
-
-git_push()
-
-
+os.chdir('Downloads')
+os.chdir('ScreenScrape')
+os.system("git push https://y1music:G4Ka38gn7E3TNKv2rksJqFyt@github.com/TikTok2-0/ScreenScrape.git --all")
